13bd244de75b4831aa99d2b8162c4fe4115e846d
xlrn <ncl.yim@gmail.com>
2021-08-17 17:01:30 -0700

    attempt to lift up state, broke code

--- typequiz/src/index.js
+++ typequiz/src/index.js
@@ -11,7 +11,34 @@ function ScoreKeeper(props) {
 }
 
 
-function SomeButton () {
+function SomeButton (props) {
+
+  useEffect(() => {
+    props.fetchData();
+  }, [])
+
+  const weaklist = props.weakness.map((weak) => 
+    <h2 key={weak}>{weak}</h2>);
+
+  if (props.error) {
+    return <div>Error: {props.error.message}</div>
+  } else if (!props.isLoaded) {
+    return <div>Loading...</div>
+  } else {
+  return (
+    <div>
+      <h1>{props.type}</h1>
+      <div>
+        {weaklist}
+      </div>
+      <button>Click here to change type</button>
+    </div>
+  )}
+}
+
+function Quiz() {
+  // lift state from somebutton into quiz
+  const [score, setScore] = useState(0);
   const [type, setType] = useState("");
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -38,33 +65,6 @@ function SomeButton () {
       })
   }
 
-  useEffect(() => {
-     fetchData();
-  }, [])
-
-  const weaklist = weakness.map((weak) => 
-    <h2 key={weak}>{weak}</h2>);
-
-  if (error) {
-    return <div>Error: {error.message}</div>
-  } else if (!isLoaded) {
-    return <div>Loading...</div>
-  } else {
-  return (
-    <div>
-      <h1>{type}</h1>
-      <div>
-        {weaklist}
-      </div>
-      <button onClick={fetchData}>Click here to change type</button>
-    </div>
-  )}
-}
-
-function Quiz() {
-  // lift state from somebutton into quiz
-  const [score, setScore] = useState(0);
-  
   function handleClick() {
     let newScore = score + 1;
     setScore(newScore);
@@ -73,7 +73,7 @@ function Quiz() {
   return (
     <div>
       <ScoreKeeper score={score}/>
-      <SomeButton/>
+      <SomeButton type={type} error={error} isLoaded={isLoaded} weakness={weakness}/>
       <button onClick={handleClick}>AAAA</button>
     </div>
   )
